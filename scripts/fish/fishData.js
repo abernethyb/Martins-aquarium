57beeca54365e3a12093a09b8d84b9e1a2eb44b2
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-06-02 11:19:36 -0500

    made fish scripts

---
+++ scripts/fish/fishData.js
@@ -0,0 +1,40 @@
+
+const fish = [
+    {
+        name: "Hercule",
+        species: "Krill"
+        length: "0.1",
+        location: "Florida",
+        food: "Not much"
+    },
+    {
+        name: "Bob",
+        species: "Clownfish",
+        length: "1"
+        location: "Florida",
+        food: "Krill"
+    },
+    {
+        name: "Carl",
+        species: "Squid",
+        length: "4"
+        location: "Washington Coast",
+        food: "Clownfish"
+    },
+    {
+        name: "Rodger",
+        species: "Hammer Head Shark",
+        length: "10"
+        location: "Georgia",
+        food: "Clownfish, Squid"
+    },
+    {
+        name: "Eight Ball",
+        species: "Octopus",
+        length: "Unknown"
+        location: "Right Behind You",
+        food: "Clownfish, Rodger, Carl, Yournamehere"
+    }
+]
+
+
