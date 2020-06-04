8e40a9b88be3c820dba5caafcf91614e411bc0ab
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-06-04 12:29:24 -0500

    transfered fish, locations, tips, and quotes data to api/database.json and fetched data back into app

--- scripts/locations/locationsData.js
+++ scripts/locations/locationsData.js
@@ -1,34 +1,16 @@
-const locationsCollection = [
-    {
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR3_Wyg4CrX-66HzUnipi_iNR4m-jw4yh6rRltuf3pnSsTpXyyX&usqp=CAU",
-        name: "Florida",
-        climate: "Sub-tropical",
-        regulations: "none",
-        gear: "SCUBA",
-        remarks: "Watch out for Florida Man and big swirly storms"
-    },
-    {
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTFCt2Js-AU1fsQ0qnmLa04-JXdOyXNxh0e-flE77n3rnIh5dRG&usqp=CAU",
-        name: "mediterranean sea",
-        climate: "Kinda Warm",
-        regulations: "That depends on what part of the sea you're in.  Be sure to check with local governments here.",
-        gear: "SCUBA, snorkel",
-        remarks: "I'd still keep an eye out for Florida Man"
-    },
-    {
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR3_Wyg4CrX-66HzUnipi_iNR4m-jw4yh6rRltuf3pnSsTpXyyX&usqp=CAU",
-        name: "Florida",
-        climate: "Sub-tropical",
-        regulations: "none",
-        gear: "SCUBA",
-        remarks: "Watch out for Florida Man and big swirly storms"
-    },
-    {
-        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR3_Wyg4CrX-66HzUnipi_iNR4m-jw4yh6rRltuf3pnSsTpXyyX&usqp=CAU",
-        name: "Florida",
-        climate: "Sub-tropical",
-        regulations: "none",
-        gear: "SCUBA",
-        remarks: "Watch out for Florida Man and big swirly storms"
-    },
-]
+let locationsCollection = []
+
+//getting data from json server
+
+const getLocationData = () => {
+    return fetch("http://localhost:8088/locations").then(
+        (response) => {
+            return response.json()
+        }
+    )
+    .then(
+        (arrayOfLocations) => {
+            locationsCollection = arrayOfLocations
+        }
+    )
+}
--- scripts/quotes/quotesData.js
+++ scripts/quotes/quotesData.js
@@ -1,18 +1,16 @@
-const quotesCollection = [
-    {
-        words: "I'm baby small batch raw denim four dollar toast succulents mustache leggings, man braid plaid pug vinyl edison bulb.",
-        author: "Virgil",
-    },
-    {
-        words: "I'm baby mlkshk plaid cronut, aesthetic swag green juice actually mollit health goth brooklyn activated charcoal.",
-        author: "Homer",
-    },
-    {
-        words: "Microdosing ut readymade tilde consequat squid literally shaman ipsum paleo bitters art party pariatur.",
-        author: "Joyce",
-    },
-    {
-        words: "Tbh mustache af pitchfork narwhal. Typewriter dolor sunt echo park gluten-free retro minim mixtape meditation ex.",
-        author: "Hot Chicken",
-    },
-]
+let quotesCollection = []
+
+//getting data from json server
+
+const getQuoteData = () => {
+    return fetch("http://localhost:8088/quotes").then(
+        (response) => {
+            return response.json()
+        }
+    )
+    .then(
+        (arrayOfQuotes) => {
+            quotesCollection = arrayOfQuotes
+        }
+    )
+}
